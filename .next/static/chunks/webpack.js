2e9675eef131386ee0fa5e856aa29ad92393ab5f
andrew11cdt <andrew11cdt@gmail.com>
2021-05-14 20:08:08 +0700

    change git name

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "13f7e8b2a93ccc52c01e"; }
+/******/ 		__webpack_require__.h = function() { return "977e35a78c8a468cdd33"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
